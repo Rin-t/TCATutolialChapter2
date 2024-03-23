dadef8a0c83abfbb3e52ea71f986d5a343312807
Rin-t <rin.t.4921@gmail.com>
2024-03-23 15:09:26 +0900

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Rin on 2024/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
